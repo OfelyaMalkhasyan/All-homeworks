70daf44129b09e520a5b1d1e5524a8105c922daf
Ofelya Malkhasyan <ofelya.malkhasyan96@gmail.com>
2021-05-22 10:17:29 +0400

    feat:solveRegexTasks

--- Lesson 014/Task3.js
+++
@@ -1,6 +0,0 @@
-let obj = Object.create(null);
-
-let key = ("What's the key?", "__proto__");
-obj[key] = "some value";
-
-console.log(obj[key]);
--- Lesson 014/TasksRegex.js
+++ Lesson 014/TasksRegex.js
@@ -1,6 +1,12 @@
+// Task 1
+const myRegExp = /^\d{6}$|^\d{4}$/;
+
+console.log(myRegExp.test("1234"));
+
+// Task 2
 let str = '<> <a href="/"> <input type="radio" checked> <b>';
 
-const REGEXP = /<\w .+>/g;
+const REGEXP = /<\w.*?>/g;
 
 console.log(str.match(REGEXP));
 //'<a href="/">', '<input type="radio" checked>', "<b>";
